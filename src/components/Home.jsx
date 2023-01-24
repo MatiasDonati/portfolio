d4dbb705b3c3d849d6cc39a5615ffacc0d284f03
MatiasDonati <matod87@hotmail.com>
2023-01-23 21:05:14 -0300

    ver trabajos en home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import { Link } from 'react-scroll'
 
 const Home = () => {
   return (
@@ -11,11 +12,15 @@ const Home = () => {
         <h2 className='ext-4xl sm:text-7xl font-bold text-[#8892b0]'>Full Stack Developer</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sit nam voluptas sed ipsam fugit similique recusandae provident dolorem exercitationem corporis voluptatem.</p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>Ver Trabajos
-            <span className='group-hover:rotate-90 duration-300'>
-              <HiArrowNarrowRight className='ml-3' />
-            </span>
-          </button>
+          <Link to="works" smooth={true} duration={500}>
+            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>Ver Trabajos
+              <span className='group-hover:rotate-90 duration-300'>
+                <HiArrowNarrowRight className='ml-3' />
+              </span>
+            </button>
+
+          </Link>
+
         </div>
       </div>
     </div>
