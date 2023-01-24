8861a692cdf53b80af2e06dc637c30b305d4a25b
MatiasDonati <matod87@hotmail.com>
2023-01-23 22:15:14 -0300

    padding top Works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,7 +6,7 @@ import Traductor from '../assets/projects/traductor.jpg'
 
 const Works = () => {
     return (
-        <div name='works' className='w-full md:h-screen text-gray-300  bg-[#0a192f]'>
+        <div name='works' className='w-full md:h-screen text-gray-300  bg-[#0a192f] pt-4'>
             <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Trabajos</p>
