021537291a9bcfdeffd193fb4f4e25d06fb937ab
MatiasDonati <matod87@hotmail.com>
2023-02-01 07:21:34 -0300

    foodApp

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 const Contact = () => {
     return (
-        <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items items-center p-4'>
+        <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items p-4'>
             <form method='POST' action="https://getform.io/f/06799291-97c1-4706-8d67-f1722a54d4f5" className='flex flex-col max-w-[600px] w-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contacto</p>
@@ -9,7 +9,7 @@ const Contact = () => {
                 <input className='bg-[#ccd6f6] p-2' type='text' placeholder='Nombre' name='name'/>
                 <input className='my-4 p-2 bg-[#ccd6f6]' type='email' placeholder='Email' name='email'/>
                 <textarea className='bg-[#ccd6f6] p-2' name='message' rows='10' placeholder='Mensaje'></textarea>
-                <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Colabora conmigo !</button>
+                <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Colabora con migo !</button>
             </form>
         </div>
     )
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,6 +3,7 @@ import RaealState from '../assets/projects/realestate.jpg'
 import Yanilog from '../assets/projects/yanilog1.jpg'
 import Teclados from '../assets/projects/teclados1.jpg'
 import Traductor from '../assets/projects/traductor.jpg'
+import FoodReactApp from '../assets/projects/FoodReactApp.png'
 
 const Works = () => {
     return (
@@ -56,7 +57,7 @@ const Works = () => {
                         </div>
                     </div>
                     {/* Grid Item */}
-                    <div style={{ backgroundImage: `url(${Yanilog})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    <div style={{ backgroundImage: `url(${FoodReactApp})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                         {/* Hover Effects */}
                         <div className='opacity-0 group-hover:opacity-100'>
@@ -64,10 +65,10 @@ const Works = () => {
                                 React JS App
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href="https://yanilog.vercel.app/" target="_blank">
+                                <a href="https://food-react-app-matiasdonati.vercel.app/" target="_blank">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
-                                <a href="https://github.com/MatiasDonati/PreEntrega1-Donati" target="_blank">
+                                <a href="https://github.com/MatiasDonati/foodReactApp" target="_blank">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                                 </a>
                             </div>
