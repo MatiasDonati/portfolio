4d656bc8e6efbd6dcb085486eaf0aae201295436
MatiasDonati <matod87@hotmail.com>
2023-03-12 20:33:53 -0300

    detalles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,10 +15,10 @@ const About = () => {
         </div>
         <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
           <div className='sm:text-right text-4xl font-bold'>
-            <p>Hola, soy Matias Donati, un gusto conocerte. Hechate un vistazo.</p>
+            <p>Hola, soy Matias Donati, un gusto conocerte. Date una vuelta.</p>
           </div>
           <div>
-            <p>Me apasiona construir softwares que mejoren la vida de los que me rodean. Me especializo en la creación de software para clientes que van desde individuos y pequeñas empresas hasta a las grandes corporaciones empresariales. ¿Qué harías si tuvieras un experto en software disponible a su alcance?</p>
+            <p>Me apasiona construir softwares que mejoren la vida de los que me rodean. Me especializo en la creación de software para clientes que van desde individuos y pequeñas empresas hasta a las grandes corporaciones empresariales. ¿Qué harías si tuvieras un experto en software disponible a tu alcance?</p>
           </div>
         </div>
       </div>
