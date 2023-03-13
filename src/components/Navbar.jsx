a9459203725aa5b5da84b79356d5729d7948a0e1
MatiasDonati <matod87@hotmail.com>
2023-03-12 23:14:47 -0300

    logo lleva a home y cursor pointer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,9 @@ const Navbar = () => {
         <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
             <div>
                 {/* <img src={Logo1} alt="Logo Image" style={{ width: '50px' }} /> */}
-                <img src={Logo2} alt="Logo Image" style={{ width: '70px' }} />
+                <Link to="home" smooth={true} duration={1500}>
+                    <img className='cursor-pointer' src={Logo2} alt="Logo Image" style={{ width: '70px' }} />
+                </Link>
             </div>
 
             {/* Menu */}
