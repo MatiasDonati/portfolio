4ab9c81b762310ffb45702d82e05108b3b7a39d7
MatiasDonati <matod87@hotmail.com>
2023-03-14 18:18:35 -0300

    correccion textarea

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,8 +11,8 @@ const Contact = () => {
                 <input className='bg-[#ccd6f6] p-2' type='text' placeholder='Nombre' name='name' />
                 <input className='my-4 p-2 bg-[#ccd6f6]' type='email' placeholder='Email' name='email' />
                 <textarea className='bg-[#ccd6f6] p-2' name='message' rows='10' placeholder='Mensaje'></textarea>
-                <button className='text-white border-2 hover:bg-[#503CD1] hover:border-[#503CD1] px-4 py-3 my-8 mx-auto flex items-center'>Escribime!</button>
-                <div className='text-white px-4 py-3 my-8 mx-auto flex items-center'>
+                <button className='text-white border-2 hover:bg-[#503CD1] hover:border-[#503CD1] px-4 py-3 my-4 mx-auto flex items-center'>Escribime!</button>
+                <div className='text-white px-4 py-3 my-[5px] mx-auto flex items-center'>
                     <img src={Logo1} alt="Logo Image" style={{ width: '120px' }} />
                 </div>
             </form>
