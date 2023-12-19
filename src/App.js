574d70859aa9337036452318ea03df13b6a5ddb2
MatiasDonati <matod87@hotmail.com>
2023-12-19 19:11:44 -0300

    icono y nombre

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
@@ -6,6 +7,16 @@ import Skills from "./components/Skills";
 import Works from "./components/Works";
 
 function App() {
+  useEffect(() => {
+    // Cambiar el título de la pestaña cuando la aplicación se monta
+    document.title = 'Matias Donati';
+
+    // Asegurarse de restaurar el título original al desmontar la aplicación
+    return () => {
+      document.title = 'Matias Donati';
+    };
+  }, []); // El array vacío asegura que el efecto solo se ejecute al montar y desmontar la aplicación
+
   return (
     <div>
       <Navbar />
